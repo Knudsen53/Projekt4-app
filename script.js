a617c839f9942bd995135648c333dd7ae995d548
Knudsen53 <frederikknudsen132@gmail.com>
2023-04-19 15:58:46 +0200

    d

--- script.js
+++ script.js
@@ -31,4 +31,28 @@ showSlides(slideIndex);
 
 //* introduktion slut *//
 
-
+function showSlides() {
+  console.log("showSlides() called");
+  window.addEventListener('DOMContentLoaded', function() {
+    var images = [
+      "Billeder/mandag.png",
+      "Billeder/tirsdag.png",
+      "Billeder/onsdag.png",
+      "Billeder/torsdag.png",
+    ];
+    
+    var currentIndex = 0;
+    
+    function nextImage() {
+      currentIndex++;
+      if (currentIndex >= images.length) {
+        currentIndex = 0;
+      }
+      var slideshow = document.getElementById("slideshow");
+      slideshow.src = images[currentIndex];
+    }
+    
+    setInterval(nextImage, 5000); // Change image every 3 seconds
+    
+  });
+}
